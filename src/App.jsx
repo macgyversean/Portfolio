36dc8f29d92186cc18630102c0e716b9291b4f64
Sean Carroll <macgyversean@gmail.com>
2024-04-02 13:53:31 -0400

    added to repo

---
+++ src/App.jsx
@@ -0,0 +1,34 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import MyThree from "./cube";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Sean Carroll</h1>
+        <p>Full Stack Developer</p>
+        <button
+          onClick={() => (window.location.href = "https://seanexperience.com")}
+        >
+          View Portfolio
+        </button>
+        <div
+          className="canvas"
+          style={{
+            maxHeight: "678px",
+            maxWidth: "1080px",
+          }}
+        >
+          <MyThree />
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
